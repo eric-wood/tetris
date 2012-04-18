5353f124599e81f95e4bd6093bbee52e6a4dd861
Eric Wood <eric@woodtx.com>
2012-04-17 22:39:09 -0500

    Some stuff

--- tetris.rb
+++ tetris.rb
@@ -1,35 +1,53 @@
 require 'curses'
 
-width  = Curses.cols
-height = Curses.lines
-posX = 0
-posY = 0
-
-PIECE = <<PIECE
+PIECE1 = <<PIECE
 OO
 OO
 OOOOOO
 OOOOOO
 PIECE
 
-def onsig(sig)
-  close_screen
-  exit(sig)
+PIECE2 = <<PIECE
+    OO
+    OO
+OOOOOO
+OOOOOO
+PIECE
+
+width  = Curses.cols
+height = Curses.lines
+posX = 0
+posY = 0
+@piece = PIECE1
+
+def write(x, y, text)
+  Curses.setpos(y, x)
+  Curses.addstr(text)
 end
 
-for i in 1 .. 15  # SIGHUP .. SIGTERM
-  if trap(i, "SIG_IGN") != 0 then  # 0 for SIG_IGN
-    trap(i) {|sig| onsig(sig) }
+def draw_piece(x, y, piece)
+  blocks = piece.split.map { |i| i.split(//) }
+  blocks.each_with_index do |row,i|
+    row.each_with_index do |c,j|
+      write(y-j, x+i, c)
+    end
   end
 end
 
-def write(line, col, text)
-  Curses.setpos(line, col)
-  Curses.addstr(text)
+def move_piece(dx, dy)
+  p dx
+  p dy
+  posX = 0 if posX + dx < 0
+  posY = 0 if posY + dy < 0
+  return nil if dx == 0 || dy == 0
+
+  Curses.clear
+  draw_piece(posX, posY, @piece)
 end
 
 def init_screen
   Curses.noecho
+  Curses.curs_set(0)
   Curses.init_screen
   Curses.stdscr.keypad(true)
   begin
@@ -40,15 +58,16 @@ def init_screen
 end
 
 init_screen do
+  draw_piece(posX, posY, @piece)
+  
   loop do
     Curses.refresh
-    write(posX,posY,PIECE)
     
     case Curses.getch
-      when Curses::Key::UP then    posX -= 1
-      when Curses::Key::DOWN then  posX += 1
-      when Curses::Key::LEFT then  posY -= 1
-      when Curses::Key::RIGHT then posY += 1
+      when Curses::Key::UP    then move_piece(0, -1)
+      when Curses::Key::DOWN  then move_piece(0, 1)
+      when Curses::Key::LEFT  then move_piece(-1, 0)
+      when Curses::Key::RIGHT then move_piece(1, 0)
     end
   end
 end
